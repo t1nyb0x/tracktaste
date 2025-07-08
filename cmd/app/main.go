dd876072a3cddf294a0b85814d25cfc41b88cb2c
t1nyb0x <dev.t1nyb0x@gmail.com>
2025-07-08 20:08:20 +0900

    APIサーバーとしてリクエスト待ち受ける形に変更

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,21 +3,46 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/t1nyb0x/tracktaste/httpclient"
 )
 
 func main() {
+    r := mux.NewRouter()
+
+    // route
+    r.HandleFunc("/fetch-artist", FetchArtistHandler).Methods("GET")
+
+    log.Println("サーバー起動中: http://localhost:8080")
+    if err := http.ListenAndServe(":8080", r); err != nil {
+        log.Fatalf("サーバー起動に失敗しました: %v", err)
+    }
+    log.Println("サーバー終了")
+}
+
+func TokenHandler(w http.ResponseWriter) string {
     accessToken, err := httpclient.GetAccessToken()
     if err != nil {
-        log.Fatal(err)
+        http.Error(w, fmt.Sprintf("Error getting access token: %v", err), http.StatusInternalServerError)
+        return ""
     }
-    
+
+    return accessToken
+}
+
+func FetchArtistHandler(w http.ResponseWriter, r *http.Request) {
+    accessToken := TokenHandler(w)
+    if accessToken == "" {
+        return
+    }
+
     body, err := httpclient.Fetch("https://api.spotify.com/v1/artists/1bY7QMGccPmba1f1frZ8Xb", accessToken)
     if err != nil {
-        log.Fatal(err)
+        http.Error(w, fmt.Sprintf("Error fetching data: %v", err), http.StatusInternalServerError)
+        return
     }
-    
-    fmt.Println("Response:")
-    fmt.Println(body)
+
+    w.Write([]byte(body))
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.24.4
 
 require github.com/joho/godotenv v1.5.1
 
-require golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da // indirect
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da // indirect
+)
